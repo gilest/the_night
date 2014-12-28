e0f9d86aacc0bc2389882154f601da4e9c788971
Giles Thompson <giles@papaiti.com>
2014-12-28 16:58:23 +1300

    Created a very basic sign up/sign in.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'nights#index'
+  root 'nights#index', as: :index
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'ß
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :nights
+  resources :users
 
   # Example resource route with options:
   #   resources :products do
