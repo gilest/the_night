322d4032cc8a5da7487244dfffbcd0e8ce0d0967
Giles Thompson <giles@papaiti.com>
2015-02-23 19:33:42 +1300

    Updated Facebook credentials.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1541839836087425', '7c761dbe002385c851d00a88bbb5f62d'
+  if Rails.env.production?
+    provider :facebook, '1532898173648258', 'ade5eae11b0d003dc006f37e117fadf9'
+  else
+    provider :facebook, '1541839836087425', '7c761dbe002385c851d00a88bbb5f62d'
+  end
 end
