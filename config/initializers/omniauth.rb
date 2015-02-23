2fb4e6c4e19a5a8a3b52a96cc22bdfef80f3c976
George Feast-Parker <georgefeast@gmail.com>
2015-02-23 19:23:10 +1300

    styley mcstyles

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1532898173648258', 'ade5eae11b0d003dc006f37e117fadf9'
+  provider :facebook, '1541839836087425', '7c761dbe002385c851d00a88bbb5f62d'
 end
